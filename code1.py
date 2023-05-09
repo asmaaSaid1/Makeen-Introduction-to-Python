2feae0287f8cb7a2a00826684e74b11c5089b961
asmaaSaid1 <130406417+asmaaSaid1@users.noreply.github.com>
2023-05-09 13:31:49 +0400

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,36 @@
+#import math library
+import math
+b=2
+n=1
+r=100
+
+exp=b*((1+r/100)**n) #arthmetic operation
+print(exp)
+exp2=b*1+r/100**n 
+print(exp2)
+
+
+print((7*(10-5)%3)*4+9)
+
+a=3
+b=1
+t=0
+#use squ(),cos()
+c=math.sqrt(a**2+b**2-2*a*b*math.cos(t))
+print(c)
+            
+  
+baisas=1729
+omr=baisas//1000
+baisas=baisas%1000 #reminder
+print(omr,baisas)
+
+
+
+price=4.35
+quantity=100
+total=(price*quantity)         
+print(round(434.999,2))
+print(int(total*100)/100)
+print("%.f"%total)
+
