7e015dc2fc447afbc11b1bbdebcabfe077446248
asmaaSaid1 <130406417+asmaaSaid1@users.noreply.github.com>
2023-05-10 12:38:50 +0400

    Add files via upload

---
+++ mid char.py
@@ -0,0 +1,13 @@
+
+#find middle character from string
+string=input("enter string:")
+
+#find middle character if string is even
+if len(string)%2==0:
+    mid=len(string)//2-1
+    print(string[mid]+string[mid+1])
+    
+#find middle character if tring is even
+else:
+    mid=(len(string)+1)//2-1
+    print(string[mid])
