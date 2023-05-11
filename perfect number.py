3209c3d8084d4bcc3636316dcfd3259ade285476
asmaaSaid1 <130406417+asmaaSaid1@users.noreply.github.com>
2023-05-11 14:16:51 +0400

    Add files via upload

---
+++ perfect number.py
@@ -0,0 +1,20 @@
+
+n=0
+for i in range(1,101):
+    n=0
+    for j in range(1,i):
+        if i%j==0:
+            
+            n=n+j
+            
+    if n==i:
+        print(i)
+                
+               
+   
+
+        
+    
+    
+        
+       
