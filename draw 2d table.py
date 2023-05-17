dcd71662b82acb87ab345e6bc4a4ca29e9051712
asmaaSaid1 <130406417+asmaaSaid1@users.noreply.github.com>
2023-05-17 13:48:05 +0400

    Add files via upload

---
+++ draw 2d table.py
@@ -0,0 +1,33 @@
+m=[[0]*4 for i in range(0,4)]
+  
+for i in range(4):
+    for j in range(4):
+        if(i==j):
+            m[i][j]=1
+        
+        elif(i>j):
+            m[i][j]=2
+    
+for i in range(4):
+    for j in range(4):
+         print(m[i][j],end=" ")
+    print()
+    
+  
+  
+print("\n")
+a=[[0]*4 for i in range(0,4)]
+
+
+for i in range(4):
+    for j in range(4):
+        if (i+j)%2==0:
+            a[i][j]=1
+     
+     
+for i in range(4):
+    for j in range(4):
+         print(a[i][j],end=" ")
+    print()
+    
+
